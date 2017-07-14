5d2cef920a4f32cc9a609064eadc39db7b2d7b9e
Gleb Gorelov <gorelov@haulmont.com>
2017-07-14 15:15:56 +0400

    Add second filter field

--- modules/web/src/com/company/sample/web/customer/CustomerBrowse.java
+++ modules/web/src/com/company/sample/web/customer/CustomerBrowse.java
@@ -1,7 +1,6 @@
 package com.company.sample.web.customer;
 
 import com.company.sample.entity.Customer;
-import com.haulmont.bali.util.ParamsMap;
 import com.haulmont.cuba.gui.components.AbstractLookup;
 import com.haulmont.cuba.gui.components.DataGrid;
 import com.haulmont.cuba.gui.components.TextField;
@@ -9,6 +8,7 @@ import com.haulmont.cuba.gui.data.CollectionDatasource;
 import com.haulmont.cuba.gui.xml.layout.ComponentsFactory;
 
 import javax.inject.Inject;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
@@ -22,13 +22,24 @@ public class CustomerBrowse extends AbstractLookup {
 
     @Override
     public void init(Map<String, Object> params) {
+        TextField nameFilter = createTextFieldFilter("name");
+        TextField emailFilter = createTextFieldFilter("email");
+
+        DataGrid.HeaderRow filterRow = customersDataGrid.appendHeaderRow();
+        filterRow.getCell("name").setComponent(nameFilter);
+        filterRow.getCell("email").setComponent(emailFilter);
+    }
+
+    private TextField createTextFieldFilter(String property) {
         TextField textField = componentsFactory.createComponent(TextField.class);
         textField.setWidthFull();
         textField.setHeight("25px");
-        textField.addTextChangeListener(event ->
-                customersDs.refresh(ParamsMap.of("name", "(?i)%" + event.getText() + "%")));
+        textField.addTextChangeListener(event -> {
+            Map<String, Object> params = new HashMap<>(customersDs.getLastRefreshParameters());
+            params.put(property, event.getText());
+            customersDs.refresh(params);
+        });
 
-        DataGrid.HeaderRow filterRow = customersDataGrid.appendHeaderRow();
-        filterRow.getCell("name").setComponent(textField);
+        return textField;
     }
 }
